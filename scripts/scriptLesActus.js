42716b8b889f2396311118e776b1ea6b33c24e91
Sébastien Werr <werr.sebastien@gmail.com>
2022-11-01 04:16:18 +0100

    site final

--- scripts/scriptLesActus.js
+++ scripts/scriptLesActus.js
@@ -15,7 +15,40 @@ function init(){
 
     $(".lireLarticle4").mouseover(chevronBleu4);
     $(".lireLarticle4").mouseout(chevronBlanc4);
+
+    $(".lireLarticle5").mouseover(chevronBleu5);
+    $(".lireLarticle5").mouseout(chevronBlanc5);
+
+    $(".lireLarticle6").mouseover(chevronBleu6);
+    $(".lireLarticle6").mouseout(chevronBlanc6);
+
+    $(".lireLarticle7").mouseover(chevronBleu7);
+    $(".lireLarticle7").mouseout(chevronBlanc7);
+
+    $(".lireLarticle8").mouseover(chevronBleu8);
+    $(".lireLarticle8").mouseout(chevronBlanc8);
+
+    $(".article").mouseover(zoomImg);
+    $(".article").mouseout(dezoomImg);
+
 }
+
+function zoomImg(){
+    let numArticle = $(this).attr('id');
+
+    numArticle = numArticle.replace(/[^0-9]/g,'');
+
+    $("#a"+numArticle+"ImgContent").css("transform", "scale(1.2)");
+}
+
+function dezoomImg(){
+    let numArticle = $(this).attr('id');
+
+    numArticle = numArticle.replace(/[^0-9]/g,'');
+
+    $("#a"+numArticle+"ImgContent").css("transform", "scale(1)");
+}
+
 
 function chevronBleu1(){
     $(".chevron1").css("background-image", "url(\"../images/les_actus/chevronBleu.png\")");
@@ -47,4 +80,36 @@ function chevronBleu4(){
 
 function chevronBlanc4(){
     $(".chevron4").css("background-image", "url(\"../images/les_actus/chevronBlanc.png\")");
+}
+
+function chevronBleu5(){
+    $(".chevron5").css("background-image", "url(\"../images/les_actus/chevronBleu.png\")");
+}
+
+function chevronBlanc5(){
+    $(".chevron5").css("background-image", "url(\"../images/les_actus/chevronBlanc.png\")");
+}
+
+function chevronBleu6(){
+    $(".chevron6").css("background-image", "url(\"../images/les_actus/chevronBleu.png\")");
+}
+
+function chevronBlanc6(){
+    $(".chevron6").css("background-image", "url(\"../images/les_actus/chevronBlanc.png\")");
+}
+
+function chevronBleu7(){
+    $(".chevron7").css("background-image", "url(\"../images/les_actus/chevronBleu.png\")");
+}
+
+function chevronBlanc7(){
+    $(".chevron7").css("background-image", "url(\"../images/les_actus/chevronBlanc.png\")");
+}
+
+function chevronBleu8(){
+    $(".chevron8").css("background-image", "url(\"../images/les_actus/chevronBleu.png\")");
+}
+
+function chevronBlanc8(){
+    $(".chevron8").css("background-image", "url(\"../images/les_actus/chevronBlanc.png\")");
 }
